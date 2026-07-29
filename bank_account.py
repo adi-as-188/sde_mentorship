c74e8a40f60f62646df2342b61db91034c25c508
Aditya Shukla <241288227+adi-as-188@users.noreply.github.com>
2026-07-29 11:22:08 +0000

    Added BankAccount class

---
+++ bank_account.py
@@ -0,0 +1,27 @@
+class BankAccount:
+    def __init__(self, account_holder, balance = 0.0):
+        self.holder = account_holder
+        self.balance = balance
+        print(f"Account Created: {self.holder} - ${self.balance} | {id(self)}")
+
+    def deposit(self, amount):
+        if amount >= 0:
+            self.balance += amount
+
+    def withdraw(self, amount):
+        if amount >= 0:
+            if self.balance >= amount:
+                self.balance -= amount
+            else:
+                print("Insufficient funds")
+
+    def display_balance(self):
+        print(f"Bank Account: {self.holder} - ${self.balance}")
+
+
+my_account = BankAccount("Aditya", 100.0)
+print(id(my_account))
+my_account.deposit(50)
+my_account.withdraw(200)
+my_account.withdraw(50)
+my_account.display_balance()
